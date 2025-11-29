c8a2ef9fd16e93dafe95ec9c0a09ea4a000566b6
Abdullah Ahmad Al-tamh <130320056+Abdullah-Al-Tamh@users.noreply.github.com>
2025-11-29 21:11:25 +0300

    Add SQL scripts for bronze loads and dimension loads (fixed)

---
+++ scripts/DML/load_data_to_tables.sql
@@ -0,0 +1,12 @@
+\c datawarehouse;
+-- Load data into dimension tables
+
+-- CRM Source Data
+COPY bronze.crm_cust_info FROM 'datasets/source_crm/cust_info.csv' WITH (FORMAT csv, HEADER true);
+COPY bronze.crm_prd_info FROM 'datasets/source_crm/prd_info.csv' WITH (FORMAT csv, HEADER true);
+COPY bronze.crm_sales_details FROM 'datasets/source_crm/sales_details.csv' WITH (FORMAT csv, HEADER true);
+
+-- ERP Source Data
+COPY bronze.erp_cust_az12 FROM 'datasets/source_erp/cust_az12.csv' WITH (FORMAT csv, HEADER true);
+COPY bronze.erp_loc_a101 FROM 'datasets/source_erp/loc_a101.csv' WITH (FORMAT csv, HEADER true);
+COPY bronze.erp_px_cat_g1v2 FROM 'datasets/source_erp/px_cat_g1v2.csv' WITH (FORMAT csv, HEADER true);
