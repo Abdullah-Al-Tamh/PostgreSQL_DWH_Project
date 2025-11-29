c311cae8259ecf6a58d56196f5870629bdc8b362
Abdullah Ahmad Al-tamh <130320056+Abdullah-Al-Tamh@users.noreply.github.com>
2025-11-29 21:47:17 +0300

    Enhance bronze ETL script with improved logging and execution tracking

--- scripts/DML/load_data_to_tables.sql
+++ scripts/DML/load_data_to_tables.sql
@@ -1,22 +1,94 @@
+\echo '============================================================'
+\echo '        BRONZE DATA LOAD - STARTING (LOCAL ETL RUN)'
+\echo '============================================================'
+\echo ''
+\echo 'Target Database: :DBNAME'
+\echo 'Started at:      ' `date`
+\echo ''
+
 \c datawarehouse;
--- Load data into dimension tables
+
+\echo '------------------------------------------------------------'
+\echo 'Loading CRM SOURCE DATA into bronze...'
+\echo '------------------------------------------------------------'
+\echo ''
 
 -- CRM Source Data
+
+\echo '[CRM] Loading crm_cust_info...'
+\echo ' - Truncating bronze.crm_cust_info'
 TRUNCATE TABLE bronze.crm_cust_info;
-COPY bronze.crm_cust_info FROM 'datasets/source_crm/cust_info.csv' WITH (FORMAT csv, HEADER true);
+\echo ' - Copying from datasets/source_crm/cust_info.csv'
+COPY bronze.crm_cust_info
+FROM 'datasets/source_crm/cust_info.csv'
+      CSV HEADER;
+\echo '[CRM] crm_cust_info - DONE'
+\echo ''
 
+
+
+\echo '[CRM] Loading crm_prd_info...'
+\echo ' - Truncating bronze.crm_prd_info'
 TRUNCATE TABLE bronze.crm_prd_info;
-COPY bronze.crm_prd_info FROM 'datasets/source_crm/prd_info.csv' WITH (FORMAT csv, HEADER true);
+\echo ' - Copying from datasets/source_crm/prd_info.csv'
+COPY bronze.crm_prd_info
+FROM 'datasets/source_crm/prd_info.csv'
+      CSV HEADER;
+\echo '[CRM] crm_prd_info - DONE'
+\echo ''
 
+
+\echo '[CRM] Loading crm_sales_details...'
+\echo ' - Truncating bronze.crm_sales_details'
 TRUNCATE TABLE bronze.crm_sales_details;
-COPY bronze.crm_sales_details FROM 'datasets/source_crm/sales_details.csv' WITH (FORMAT csv, HEADER true);
+\echo ' - Copying from datasets/source_crm/sales_details.csv'
+COPY bronze.crm_sales_details
+FROM 'datasets/source_crm/sales_details.csv'
+      CSV HEADER;
+\echo '[CRM] crm_sales_details - DONE'
+\echo ''
+
 
 -- ERP Source Data
+
+
+\echo '------------------------------------------------------------'
+\echo 'Loading ERP SOURCE DATA into bronze...'
+\echo '------------------------------------------------------------'
+\echo ''
+
+
+\echo '[ERP] Loading erp_cust_az12...'
 TRUNCATE TABLE bronze.erp_cust_az12;
-COPY bronze.erp_cust_az12 FROM 'datasets/source_erp/cust_az12.csv' WITH (FORMAT csv, HEADER true);
+COPY bronze.erp_cust_az12
+FROM 'datasets/source_erp/cust_az12.csv'
+      CSV HEADER;
+\echo '[ERP] erp_cust_az12 - DONE'
+\echo ''
 
+
+
+\echo '[ERP] Loading erp_loc_a101...'
 TRUNCATE TABLE bronze.erp_loc_a101;
-COPY bronze.erp_loc_a101 FROM 'datasets/source_erp/loc_a101.csv' WITH (FORMAT csv, HEADER true);
+COPY bronze.erp_loc_a101
+FROM 'datasets/source_erp/loc_a101.csv'
+      CSV HEADER;
+\echo '[ERP] erp_loc_a101 - DONE'
+\echo ''
 
+
+
+\echo '[ERP] Loading erp_px_cat_g1v2...'
 TRUNCATE TABLE bronze.erp_px_cat_g1v2;
-COPY bronze.erp_px_cat_g1v2 FROM 'datasets/source_erp/px_cat_g1v2.csv' WITH (FORMAT csv, HEADER true);
+COPY bronze.erp_px_cat_g1v2
+FROM 'datasets/source_erp/px_cat_g1v2.csv'
+      CSV HEADER;
+\echo '[ERP] erp_px_cat_g1v2 - DONE'
+\echo ''
+
+
+\echo ''
+\echo '============================================================'
+\echo ' BRONZE DATA LOAD - COMPLETED SUCCESSFULLY'
+\echo ' Finished at: ' `date`
+\echo '============================================================'
